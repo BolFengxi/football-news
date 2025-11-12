6a4bb92726b0a1d70de19ad02d3ca281aa0193a2
Iqbal <raflyiqbal1409@gmail.com>
2025-11-12 14:35:06 +0700

    Finishing Tutorial 8

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,25 @@ from django.contrib.auth import login as auth_login, authenticate
 from django.http import HttpResponseRedirect, JsonResponse
 from django.contrib.auth.models import User
 import json
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
+        }, status=401)
 
 # Create your views here.
 @csrf_exempt
